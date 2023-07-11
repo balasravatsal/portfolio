2c6a4e9281fbde15f110450305f377ca083e0b43
balasravatsal <vatsalbalasra24@gmail.com>
2023-07-12 01:53:52 +0530

    Experience start

--- src/Pages/Experience.jsx
+++ src/Pages/Experience.jsx
@@ -1,9 +1,24 @@
+import Typography from "@mui/material/Typography";
+import ExpCard from "../Components/Experience/ExpCard.jsx";
+import * as PropTypes from "prop-types";
 
+function Item() {
+    return null;
+}
+
+Item.propTypes = {children: PropTypes.node};
 const Experience = () => {
     return (
-        <main>
-            Experience
-        </main>
+        <div className={'experience'}>
+            <Typography variant={"h2"} display={"grid"} justifyContent={"center"} mb={"4rem"}>My Experience</Typography>
+
+            <divd className={"expDiv"}>
+                <ExpCard/>
+                <ExpCard/>
+                <ExpCard/>
+            </divd>
+
+        </div>
     );
 };
 
